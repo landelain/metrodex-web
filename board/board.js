a85cfd2e31dca93bb0e6e5c2693a7c7c473107d5
landelain <lulumassot22@gmail.com>
2026-06-30 19:38:22 +0200

    start of database and accounts implemented

---
+++ board/board.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, doc, setDoc, getDoc, getDocs, query, where, updateDoc } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjSOVCiCKyzYGhLEe3yATufL8KTja2g8o",
+  authDomain: "metrodex-database.firebaseapp.com",
+  projectId: "metrodex-database",
+  storageBucket: "metrodex-database.firebasestorage.app",
+  messagingSenderId: "956454868769",
+  appId: "1:956454868769:web:06da51d224ebc497b55060"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    console.log("Logged in as", user.email);
+    
+    const snap = await getDoc(doc(db, "users", user.uid));
+    if (snap.exists()) {
+        const data = snap.data();
+        console.log(data["email"]);
+}
+
+  } else {
+    // not looged in, kick back to login page
+    window.location.href = '../login/login.html';
+  }
+});
---
+++ login/login.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, doc, setDoc, getDoc, getDocs, query, where, updateDoc } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjSOVCiCKyzYGhLEe3yATufL8KTja2g8o",
+  authDomain: "metrodex-database.firebaseapp.com",
+  projectId: "metrodex-database",
+  storageBucket: "metrodex-database.firebasestorage.app",
+  messagingSenderId: "956454868769",
+  appId: "1:956454868769:web:06da51d224ebc497b55060"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById('login_button').addEventListener('click', async () => {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    const db = getFirestore(app);
+
+    await setDoc(doc(db, "users", user.uid), {
+    email: user.email,
+    joinedAt: new Date(),
+    merge: true
+    });
+
+    // only redirect AFTER login succeeds
+    window.location.href = 'board/board.html';
+
+  } catch (e) {
+    document.getElementById('loginError').textContent = e.message;
+  }
+});
+
