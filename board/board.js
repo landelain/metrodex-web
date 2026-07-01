84f7b71497c553fdb36adeacb152d1031a20afdd
landelain <lulumassot22@gmail.com>
2026-07-01 14:57:58 +0200

    test dvh

--- board/board.js
+++ board/board.js
@@ -32,52 +32,115 @@ onAuthStateChanged(auth, async (user) => {
   }
 });
 
-document.getElementById('paris_button').addEventListener('click', async () => {
 
-  const user = auth.currentUser;
 
-  // console.log("here")
-  // console.log(user)
-  // console.log(user.email)
+const select = document.getElementById("city-select");
+let city = select.options[select.selectedIndex].value;
+console.log(city)
 
-  if (!user) {
-    console.error("User not ready yet");
-    return;
+select.addEventListener("change", async () => {
+  city = select.options[select.selectedIndex].value;
+  console.log(city);
+});
+
+
+
+
+
+const stations = document.getElementById("stations");
+const max_snippets = 40;
+
+let stationsnippet = [];
+for (let i = 0 ; i < max_snippets ; i++){
+
+  let snippet_i = document.createElement("div");
+  let id = "snippet" + String(i);
+  snippet_i.setAttribute("id", id);
+  snippet_i.setAttribute("class", "snippet");
+  stations.appendChild(snippet_i);
+  stationsnippet.push(snippet_i);
+
+}
+
+let current_line_station_n = 20;
+
+
+function display_n_snippets(n) {
+
+  for (let i = 0 ; i < n ; i++){
+    stationsnippet[i].style.height = "5%";
   }
+}
 
-  await setDoc(doc(db, "users", user.uid), {
-    
-    city: "paris",
-    
-  }, { merge: true });
+function hide_snippets() {
+  for (let i = 0 ; i < max_snippets ; i++){
+    stationsnippet[i].style.height = "0%";
+  }
+}
 
-  // const snap = await getDoc(doc(db, "users", user.uid));
-  //   if (snap.exists()) {
-  //     const data = snap.data();
-  //     console.log(data["city"]);
-  //   }
+display_n_snippets(3);
 
-  window.location.href = '../main/main.html';
+const arrow = document.getElementById("arrow");
+const stationboard = document.getElementById("station-board")
+let hidden = true;
+
+arrow.addEventListener("click", async () => {
+
+  if (hidden) {
+
+    stationboard.style.height = "90%" ;
+    stationboard.style.padding = "5%";
+    stationboard.style.paddingTop="7%";
+
+    hidden = false;
+
+    arrow.textContent = "⌄";
+    arrow.style.justifyContent = "flex-start";
+    arrow.style.transform = "translateY(50%)";
+
+  } else {
+
+    stationboard.style.height = "0%";
+    stationboard.style.padding = "0%";
+    stationboard.style.paddingTop="0%";
+
+    hidden = true;
+
+    arrow.textContent = "^"
+    arrow.style.justifyContent = "flex-end";
+    arrow.style.transform = "translateY(20%)";
+
+  }
 
 });
 
 
-document.getElementById('lausanne_button').addEventListener('click', async () => {
 
-  const user = auth.currentUser;
+// document.getElementById('paris_button').addEventListener('click', async () => {
 
-  if (!user) {
-    console.error("User not ready yet");
-    return;
-  }
+//   const user = auth.currentUser;
 
+//   // console.log("here")
+//   // console.log(user)
+//   // console.log(user.email)
 
-  await setDoc(doc(db, "users", user.uid), {
+//   if (!user) {
+//     console.error("User not ready yet");
+//     return;
+//   }
+
+//   await setDoc(doc(db, "users", user.uid), {
     
-    city: "lausanne",
+//     city: "paris",
     
-  }, { merge: true });
-  
-  window.location.href = '../main/main.html';
+//   }, { merge: true });
 
-});
+//   // const snap = await getDoc(doc(db, "users", user.uid));
+//   //   if (snap.exists()) {
+//   //     const data = snap.data();
+//   //     console.log(data["city"]);
+//   //   }
+
+//   window.location.href = '../main/main.html';
+
+// });
